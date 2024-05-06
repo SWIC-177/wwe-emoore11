260ce9ded03d5f78eb3a8e0aa974c71e8e81e637
Erina <erina.moore@student.swic.edu>
2024-05-05 21:21:17 -0500

    Create passing test

--- src/lib.js
+++ src/lib.js
@@ -4,12 +4,16 @@ export function getLastName(fullName) {
 }
 
 export function merge2ArraysIntoAnArrayOfObjects({ a1, a2, key1, key2 }) {
-  // Function implementation
-}
+  return a1.reduce((acc, item) => {}, []);
+  const accumlatedResult2Modify = a1.reduce((acc, item, index) => {
+    acc.push({ [key1]: item, [key2]: a2[index] });
+    return acc;
+  } 
+
 
 export function removeCorrespondingItemsByTerm({ terms1, terms2, filterTerm }) {
   const index = terms1.indexOf(filterTerm);
   terms1.splice(index, 1);
   terms2.splice(index, 1);
   return { terms1, terms2 };
-}
+}}
